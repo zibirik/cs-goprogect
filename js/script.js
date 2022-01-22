86bd74c8866ec0ca2d71ce7a682095e5bf854f55
mitr <mitr@mitrs-macbook-pro.local>
2022-01-22 20:37:52 +0000

    ✍️ + js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () { 
+	$('#submenu').hover(
+		function () {
+			$('.nav_sub', this).stop().fadeIn(100);
+		}, 
+		function () {
+			$('.nav_sub', this).stop().fadeOut(100);         
+		}
+	);
+});
+
+
